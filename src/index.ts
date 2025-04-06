b34749364713ff2e021dc5a20b7fbc9f4434e499
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-04-06 12:53:33 +0200

    * change: curried functions getButton() and createButton()

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-const add = (a) => (b) => Number(a) + Number(b);
-const subtract = (a) => (b) => Number(a) - Number(b);
-const multiply = (a) => (b) => Number(a) * Number(b);
-const divide = (a) => (b) => (Number(b) != 0) ? Number(a) / Number(b) : undefined;
+const calculate = operation => a => b => operation(a)(b);
 
-const calculate = (operation) => (a) => (b) => operation(a)(b);
+const add = a => b => Number(a) + Number(b);
+const subtract = a => b => Number(a) - Number(b);
+const multiply = a => b => Number(a) * Number(b);
+const divide = a => b => (Number(b) != 0) ? Number(a) / Number(b) : undefined;
 
-const getVal = (elemId) => (<HTMLInputElement>document.getElementById(elemId)).value;
-const setResult = (elemId) => (result) => document.getElementById(elemId).innerHTML = result;
+const getVal = elemId => (<HTMLInputElement>document.getElementById(elemId)).value;
+const setResult = elemId => result => document.getElementById(elemId).innerHTML = result;
 
 const operationMaps = [
     {
@@ -27,16 +27,19 @@ const operationMaps = [
     }
 ];
 
-const getButton = (opMap, result, resId, calc, getVal, valAId, valBId) => {
+const createButton = opMaps => getButton => setResult => resId => calc => getVal => a => b => {
+    if (opMaps.length != 0) {
+    return [getButton(opMaps[0])(setResult)(resId)(calc)(getVal)(a)(b)]
+        .concat(createButton(opMaps.slice(1))(getButton)(setResult)(resId)(calc)(getVal)(a)(b));
+    }
+}
+
+const getButton = opMap => result => resId => calc => getVal => valAId => valBId => {
     const button = document.getElementById(opMap.name);
-    button.addEventListener("click", () => { result(resId)(calc(opMap.func)(getVal(valAId))(getVal(valBId))); });
+    button.addEventListener("click", () => {
+        result(resId)(calc(opMap.func)(getVal(valAId))(getVal(valBId)));
+    });
     return button;
 }
 
-const createButton = (opMaps, getButton, setResult, resId, calc, getVal, a, b) => {
-    if (opMaps.length === 0) { return; };
-    return [getButton(opMaps[0], setResult, resId, calc, getVal, a, b)]
-        .concat(createButton(opMaps.slice(1), getButton, setResult, resId, calc, getVal, a, b));
-}
-
-const buttons = createButton(operationMaps, getButton, setResult, "result", calculate, getVal, "firstValue", "secondValue");
+const buttons = createButton(operationMaps)(getButton)(setResult)("result")(calculate)(getVal)("firstValue")("secondValue");

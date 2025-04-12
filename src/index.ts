231b2c170b59ad1bbd2b19c4574822d4edfbbd12
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-04-12 16:03:23 +0200

    * change: some rearrangement of code layout

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-const calculate = operation => a => b => operation(a)(b);
+// operations
 
 const add = a => b => Number(a) + Number(b);
 const subtract = a => b => Number(a) - Number(b);
 const multiply = a => b => Number(a) * Number(b);
 const divide = a => b => (b != "0") ? Number(a) / Number(b) : undefined;
 
-const getVal = elemId => (<HTMLInputElement>document.getElementById(elemId)).value;
-const setResult = elemId => result => document.getElementById(elemId).innerHTML = result;
 
-const numpadNumbers = [
+// display
+
+const display = document.getElementById("display");
+
+const updateDisplay = state => display => {
+    display.innerHTML = state.display;
+};
+
+const showResult = state => display => {
+    display.innerHTML = String(state.operation(state.firstValue)(state.secondValue));
+};
+
+
+// number buttons
+
+const numberButtons = [
     { name: "numpad-zero", value: "0", },
     { name: "numpad-one", value: "1", },
     { name: "numpad-two", value: "2", },
@@ -22,19 +35,20 @@ const numpadNumbers = [
     // { name: "numpad-period", value: ".", },
 ];
 
-const numpadOperations = [
-    { name: "numpad-plus", function: add, },
-    { name: "numpad-minus", function: subtract, },
-    { name: "numpad-times", function: multiply, },
-    { name: "numpad-by", function: divide, },
-];
+const getNumberButton = numpadNumber => display => {
+    const numpadButton = document.getElementById(numpadNumber.name);
+    numpadButton.addEventListener("click", () => {
+        calcState = inputValue(calcState, numpadNumber.value);
+        updateDisplay(calcState)(display);
+    });
+    return numpadButton;
+};
 
-const initialCalcState = {
-    firstValue: "",
-    secondValue: "",
-    awaitingSecondValue: false,
-    operation: null,
-    display: "",
+const createNumberButtons = numpadNumbers => display => {
+    if (numpadNumbers.length != 0) {
+        return [getNumberButton(numpadNumbers[0])(display)]
+            .concat(createNumberButtons(numpadNumbers.slice(1))(display));
+    }
 };
 
 const inputValue = (state, value) => {
@@ -44,20 +58,44 @@ const inputValue = (state, value) => {
             firstValue: state.firstValue + value,
             display: state.firstValue + value,
         }
-    }
-    if (state.awaitingSecondValue) {
+    } else {
         return {
             ...state,
             secondValue: state.secondValue + value,
             display: state.secondValue + value,
         }
     }
-    return {
-        ...state,
-    }
 };
 
-const setOperation = (state, operation) => {
+const numpadButtons = createNumberButtons(numberButtons)(display);
+
+
+// operation buttons
+
+const numpadOperations = [
+    { name: "numpad-plus", function: add, },
+    { name: "numpad-minus", function: subtract, },
+    { name: "numpad-times", function: multiply, },
+    { name: "numpad-by", function: divide, },
+];
+
+const getOperationButton = display => numpadOperation => {
+    const button = document.getElementById(numpadOperation.name);
+    button.addEventListener("click", () => {
+        calcState = setOperation(calcState)(numpadOperation);
+        updateDisplay(calcState)(display);
+    });
+    return button;
+}
+
+const createOperationButtons = display => numpadOperations => {
+    if (numpadOperations.length != 0) {
+        return [getOperationButton(display)(numpadOperations[0])]
+            .concat(createOperationButtons(display)(numpadOperations.slice(1)));
+    }
+}
+
+const setOperation = state => operation => {
     return {
         ...state,
         operation: operation.function,
@@ -66,60 +104,26 @@ const setOperation = (state, operation) => {
     }
 };
 
-const updateDisplay = (state, display) => {
-    display.innerHTML = state.display;
-};
+const numpadOperationButtons = createOperationButtons(display)(numpadOperations);
 
-const showResult = (state, display) => {
-    display.innerHTML = String(state.operation(state.firstValue)(state.secondValue));
-};
 
-let calcState = initialCalcState;
+// equals button
 
 const equalsButton = document.getElementById("numpad-equals");
 equalsButton.addEventListener("click", () => {
-    showResult(calcState, display);
+    showResult(calcState)(display);
     calcState = initialCalcState;
 });
 
-const createNumpadOperationButtons = display => numpadOperations => {
-    if (numpadOperations.length != 0) {
-        return [getNumpadOperationButton(display)(numpadOperations[0])]
-            .concat(createNumpadOperationButtons(display)(numpadOperations.slice(1)));
-    }
-}
 
-const getNumpadOperationButton = display => numpadOperation => {
-    const button = document.getElementById(numpadOperation.name);
-    button.addEventListener("click", () => {
-        calcState = setOperation(calcState, numpadOperation);
-        updateDisplay(calcState, display);
-    });
-    return button;
-}
+// init calculator
 
-const getValueAndOperation = displayValue => operation => {
-    return [displayValue, operation];
-}
-
-const createNumpadButtons = numpadNumbers => display => {
-    if (numpadNumbers.length != 0) {
-        return [getNumpadButton(numpadNumbers[0])(display)]
-            .concat(createNumpadButtons(numpadNumbers.slice(1))(display));
-    }
+const initialCalcState = {
+    firstValue: "",
+    secondValue: "",
+    awaitingSecondValue: false,
+    operation: null,
+    display: "",
 };
 
-const getNumpadButton = numpadNumber => display => {
-    const numpadButton = document.getElementById(numpadNumber.name);
-    numpadButton.addEventListener("click", () => {
-        calcState = inputValue(calcState, numpadNumber.value);
-        updateDisplay(calcState, display);
-    });
-    return numpadButton;
-};
-
-const display = document.getElementById("display");
-
-const numpadButtons = createNumpadButtons(numpadNumbers)(display);
-const numpadOperationButtons = createNumpadOperationButtons(display)(numpadOperations);
-
+let calcState = initialCalcState;
